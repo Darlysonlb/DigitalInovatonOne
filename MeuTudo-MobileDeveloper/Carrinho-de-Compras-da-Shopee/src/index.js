eb0f4e8b42c439c2c8978ae4606bf8f916fc6ca7
Darlysonlb <darlysonlb@gmail.com>
2025-07-04 22:07:31 -0300

    Primeira parte da logica

---
+++ MeuTudo-MobileDeveloper/Carrinho-de-Compras-da-Shopee/src/index.js
@@ -0,0 +1,16 @@
+import createItem from "./services/item.js"
+import * as cartSerive from "./services/cart.js"
+
+const myCart = []
+const myWhishList = []
+
+console.log("Welcome to the your car shopee")
+
+const item1 = await createItem("Toy", 20.99, 1)
+const item2 = await createItem("Toys", 39.99, 3)
+
+await cartSerive.addItem(myCart, item1)
+await cartSerive.addItem(myWhishList, item2)
+
+console.log("Shopee Cart Total")
+await cartSerive.calculateTotal(myCart)

45f1d81462e58b70999576fb223c179c73993065
Darlysonlb <darlysonlb@gmail.com>
2025-07-07 22:36:47 -0300

    Logica concluida

--- MeuTudo-MobileDeveloper/Carrinho-de-Compras-da-Shopee/src/index.js
+++ MeuTudo-MobileDeveloper/Carrinho-de-Compras-da-Shopee/src/index.js
@@ -10,7 +10,19 @@ const item1 = await createItem("Toy", 20.99, 1)
 const item2 = await createItem("Toys", 39.99, 3)
 
 await cartSerive.addItem(myCart, item1)
-await cartSerive.addItem(myWhishList, item2)
+await cartSerive.addItem(myCart, item2)
+
+await cartSerive.removeItem(myCart, item2)
+await cartSerive.removeItem(myCart, item2)
+
+await cartSerive.displaycart(myCart, item2)
+
+//await cartSerive.deleteItem(myCart, item2.name)
+//await cartSerive.deleteItem(myCart, item1.name)
+
+
+
+
 
 console.log("Shopee Cart Total")
 await cartSerive.calculateTotal(myCart)
--- MeuTudo-MobileDeveloper/Carrinho-de-Compras-da-Shopee/src/services/cart.js
+++ MeuTudo-MobileDeveloper/Carrinho-de-Compras-da-Shopee/src/services/cart.js
@@ -8,16 +8,40 @@ async function calculateTotal(userCart) {
 }
 
 async function deleteItem(userCart, name) {
+    const index = userCart.findIndex((item) => item.name === name)
+    if (index !== -1) {
+        userCart.splice(index, 1)
+    }
+}
+
+async function removeItem(userCart, item) {
+    const indexFound = userCart.findIndex((p) => p.name == item.name)
+    if (indexFound == -1) {
+        console.log("item não encontrado")
+        return;
+    }
+    if (userCart[indexFound].quantity > 1) {
+        userCart[indexFound].quantity -= 1
+        return
+    }
+    if (userCart[indexFound].quantity == 1) {
+        userCart.splice(indexFound, 1)
+        return
+    }
 
 }
 
-async function removeItem(userCart, index) {
-
+async function displaycart(userCart) {
+    console.log("Shopee cart list:")
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantity} | Subtotal = ${item.subtotal()}`)
+    })
 }
 
 export {
     addItem,
     calculateTotal,
     deleteItem,
-    removeItem
+    removeItem,
+    displaycart
 }
